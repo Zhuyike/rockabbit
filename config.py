4a249b900a4d40ed1a517d95e0a36369743c1f57
Yike Zhu <yike.zhu@happyelements.com>
2020-01-07 20:11:56 +0800

    save

--- config.py
+++ config.py
@@ -1,6 +1,24 @@
-from nonebot.default_config import *
+# from nonebot.default_config import *
+from pymongo import MongoClient
 
-SUPERUSERS = [791949127]
-COMMAND_START = ['', '/', '!', '／', '！']
+SUPERUSERS = [791949127, ]
+COMMAND_START = ['', '/', '!', '／', '！', ]
 HOST = '127.0.0.1'
 PORT = 32409
+t_keys = ['战斗吧歌姬', ]
+mongo_host = '123.57.95.42'
+mongo_port = 27017
+mongo_user = 'keientist_admin'
+mongo_pwd = 'keientist_pwd'
+
+
+def keientist_db_instance():
+    mongo_client = MongoClient(host=mongo_host, port=mongo_port)
+    mongo_client.keientist.authenticate(mongo_user, mongo_pwd)
+    dbs = dict()
+    dbs['keientist'] = mongo_client['keientist']
+    dbs['revdol_data'] = mongo_client['revdol_data']
+    return dbs
+
+
+mongo_db = keientist_db_instance()
