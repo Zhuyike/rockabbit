0545fab1b32786c2367976b9dd3e166e46bb22a0
Yike Zhu <yike.zhu@happyelements.com>
2020-01-17 10:29:42 +0800

    fix check

--- bili/plugins/video_status.py
+++ bili/plugins/video_status.py
@@ -15,16 +15,18 @@ async def weather(session: CommandSession):
         for index in range(5 if len(argv) == 0 else int(argv[0])):
             status = data['data']['list']['vlist'][index]
             av_id = status['aid']
-            data_v = await u.web_get('https://api.bilibili.com/x/web-interface/view?aid={}'.format(av_id))
-            data_v = data_v['data']['stat']
-            msg += '标题：{}\n播放数：{}\n赞：{}\n硬币：{}\n收藏：{}\n转发：{}\n\n'.format(
-                status['title'], data_v['view'], data_v['like'], data_v['coin'], data_v['favorite'], data_v['share'])
+            msg += await get_each_msg(av_id)
     else:
         for arg in argv:
             av_id = arg[2:] if 'av' in arg or 'AV' in arg else arg
-            data = await u.web_get('https://api.bilibili.com/x/web-interface/view?aid={}'.format(av_id))
-            name_data = data['data']
-            data = data['data']['stat']
-            msg += '标题：{}\n播放数：{}\n赞：{}\n硬币：{}\n收藏：{}\n转发：{}\n\n'.format(
-                name_data['title'], data['view'], data['like'], data['coin'], data['favorite'], data['share'])
+            msg += await get_each_msg(av_id)
     await session.send(msg + '当前同时观看人数仍在开发中')
+
+
+async def get_each_msg(av_id):
+    data = await u.web_get('https://api.bilibili.com/x/web-interface/view?aid={}'.format(av_id))
+    name_data = data['data']
+    data = data['data']['stat']
+    msg = '标题：{}\nhttps://www.bilibili.com/video/av{}\n播放数：{}\n赞：{}\n硬币：{}\n收藏：{}\n转发：{}\n\n'.format(
+        name_data['title'], av_id, data['view'], data['like'], data['coin'], data['favorite'], data['share'])
+    return msg
