a17efe3af208ce315789cd67251c06c4f3780b1e
zhuyike <zhuyike@bupt.edu.cn>
2020-01-19 02:18:15 +0800

    save

--- bili/plugins/check_rank.py
+++ bili/plugins/check_rank.py
@@ -9,15 +9,19 @@ async def _():
     bot = nonebot.get_bot()
     if not bot.config.event_903:
         return
-    await bot.send(context={'user_id': '791949127'}, message='在{}榜上被人超过了'.format(bot.config.idol_dict[int(1)]))
     mongo_db = bot.config.mongo_db['keientist']
     filter_ = {'type': "903_rank"}
     rank = await u.db_executor(mongo_db.cookie_secret.find_one, filter_)
     c = hyper.HTTP20Connection('starmicro.happyelements.cn', port=443, secure=True, ssl_context=None)
     for i in "123456":
         if rank[i] != "x":
-            c.request('GET', '/v2/new-year/supporter-rank?event_id=903&idol_id={}'.format(i),
-                      headers=bot.config.wechat_auth)
+            while True:
+                try:
+                    c.request('GET', '/v2/new-year/supporter-rank?event_id=903&idol_id={}'.format(i),
+                              headers=bot.config.wechat_auth)
+                    break
+                except AttributeError:
+                    pass
             data = json.loads(c.get_response().read())
             for user in data['data']:
                 if user['uid'] == bot.config.my_uid:
