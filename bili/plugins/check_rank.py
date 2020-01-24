ea122c23ecf8c57d336e0f0c1b3ec70a1e80b85b
zhuyike <zhuyike@bupt.edu.cn>
2020-01-24 11:39:03 +0800

    save

--- bili/plugins/check_rank.py
+++ bili/plugins/check_rank.py
@@ -4,35 +4,35 @@ import hyper
 import json
 
 
-@nonebot.scheduler.scheduled_job('cron', second='1', minute='*/5')
-async def _():
-    bot = nonebot.get_bot()
-    if not bot.config.event_903:
-        return
-    mongo_db = bot.config.mongo_db['keientist']
-    filter_ = {'type': "903_rank"}
-    rank = await u.db_executor(mongo_db.cookie_secret.find_one, filter_)
-    print(rank)
-    c = hyper.HTTP20Connection('starmicro.happyelements.cn', port=443, secure=True, ssl_context=None)
-    for i in "123456":
-        if rank[i] != "x":
-            while True:
-                try:
-                    c.request('GET', '/v2/new-year/supporter-rank?event_id=903&idol_id={}'.format(i),
-                              headers=bot.config.wechat_auth)
-                    break
-                except AttributeError:
-                    pass
-            data = json.loads(c.get_response().read())
-            for user in data['data']:
-                if user['uid'] == bot.config.my_uid:
-                    if user['rank'] > rank[i]:
-                        await bot.send(context={'user_id': '791949127'},
-                                       message='在{}榜上被人超过了'.format(bot.config.idol_dict[int(i)]))
-                    else:
-                        print('{}没有被超'.format(bot.config.idol_dict[int(i)]))
-                    rank[i] = user['rank']
-        else:
-            print('{}不打算监控'.format(bot.config.idol_dict[int(i)]))
-    await u.db_executor(mongo_db.cookie_secret.save, rank)
-    print("success: {}".format(rank))
+# @nonebot.scheduler.scheduled_job('cron', second='1', minute='*/5')
+# async def _():
+#     bot = nonebot.get_bot()
+#     if not bot.config.event_903:
+#         return
+#     mongo_db = bot.config.mongo_db['keientist']
+#     filter_ = {'type': "903_rank"}
+#     rank = await u.db_executor(mongo_db.cookie_secret.find_one, filter_)
+#     print(rank)
+#     c = hyper.HTTP20Connection('starmicro.happyelements.cn', port=443, secure=True, ssl_context=None)
+#     for i in "123456":
+#         if rank[i] != "x":
+#             while True:
+#                 try:
+#                     c.request('GET', '/v2/new-year/supporter-rank?event_id=903&idol_id={}'.format(i),
+#                               headers=bot.config.wechat_auth)
+#                     break
+#                 except AttributeError:
+#                     pass
+#             data = json.loads(c.get_response().read())
+#             for user in data['data']:
+#                 if user['uid'] == bot.config.my_uid:
+#                     if user['rank'] > rank[i]:
+#                         await bot.send(context={'user_id': '791949127'},
+#                                        message='在{}榜上被人超过了'.format(bot.config.idol_dict[int(i)]))
+#                     else:
+#                         print('{}没有被超'.format(bot.config.idol_dict[int(i)]))
+#                     rank[i] = user['rank']
+#         else:
+#             print('{}不打算监控'.format(bot.config.idol_dict[int(i)]))
+#     await u.db_executor(mongo_db.cookie_secret.save, rank)
+#     print("success: {}".format(rank))
--- config.py
+++ config.py
@@ -39,22 +39,22 @@ def keientist_db_instance():
     return dbs
 
 
-def get_wechat_auth():
-    headers = {
-        'content-type': 'application/x-www-form-urlencoded',
-        'x-timezone-offset': '8',
-        'accept': '*/*',
-        'authorization': 'Miinno Tx5ZBthwvPdvQH9YPzRzkvNuvB-CZ7Cc_1556905329',
-        'brand': 'iPhone',
-        'accept-language': 'zh-cn',
-        'accept-encoding': 'br, gzip, deflate',
-        'platform': '1',
-        'user-agent': 'Mozilla/5.0(iPhone; CPU iPhone OS 12_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/7.0.5(0x17000523) NetType/WIFI Language/zh_CN',
-        'referer': 'https://servicewechat.com/wx0a48c468391428fd/61/page-frame.html',
-        'model': 'iPhone 8 Plus (GSM+CDMA)<iPhone10,2>'
-    }
-    return headers
+# def get_wechat_auth():
+#     headers = {
+#         'content-type': 'application/x-www-form-urlencoded',
+#         'x-timezone-offset': '8',
+#         'accept': '*/*',
+#         'authorization': '',
+#         'brand': 'iPhone',
+#         'accept-language': 'zh-cn',
+#         'accept-encoding': 'br, gzip, deflate',
+#         'platform': '1',
+#         'user-agent': 'Mozilla/5.0(iPhone; CPU iPhone OS 12_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/7.0.5(0x17000523) NetType/WIFI Language/zh_CN',
+#         'referer': 'https://servicewechat.com/wx0a48c468391428fd/61/page-frame.html',
+#         'model': 'iPhone 8 Plus (GSM+CDMA)<iPhone10,2>'
+#     }
+#     return headers
 
 
 mongo_db = keientist_db_instance()
-wechat_auth = get_wechat_auth()
+# wechat_auth = get_wechat_auth()
