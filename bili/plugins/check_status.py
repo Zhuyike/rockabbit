caa73b9f236a368e5034edc139d552780b59ab93
zhuyike <zhuyike@bupt.edu.cn>
2020-03-24 00:59:06 +0800

    save

--- bili/plugins/check_status.py
+++ bili/plugins/check_status.py
@@ -14,40 +14,59 @@ async def _():
     for archive in data['data']['news']['archives']:
         if int(archive['owner']['mid']) in bot.config.black_list:
             continue
-        av_id = 'av' + str(archive['aid'])
+        if archive.get('aid'):
+            av_id = 'av' + str(archive['aid'])
+        else:
+            av_id = '-1'
+        bv_id = archive['bvid']
         filter_ = {'av': av_id}
         check_data = await u.db_executor(mongo_db.bili_av.find_one, filter_)
-        if check_data:
+        check_data_bv = await u.db_executor(mongo_db.bili_av.find_one, {'bv': bv_id})
+        if check_data or check_data_bv:
             pass
         else:
-            msg = await get_msg(archive['owner']['mid'], archive['owner']['name'], archive['title'], av_id, bot)
+            msg = await get_msg(archive['owner']['mid'], archive['owner']['name'], archive['title'], av_id, bot, bv_id)
             await u.batch_send_msg(bot.config.check_status_list, msg)
-            insert_data = {'av': av_id, 'read': True}
+            if av_id != '-1':
+                insert_data = {'av': av_id, 'read': True, 'bv': bv_id}
+            else:
+                insert_data = {'read': True, 'bv': bv_id}
             await u.db_executor(mongo_db.bili_av.insert, insert_data)
     for result in data_revdol['data']['result']:
         if int(result['mid']) in bot.config.black_list:
             continue
-        av_id = 'av' + str(result['aid'])
+        if result.get('aid'):
+            av_id = 'av' + str(result['aid'])
+        else:
+            av_id = '-1'
         filter_ = {'av': av_id}
+        bv_id = result['bvid']
         check_data = await u.db_executor(mongo_db.bili_av.find_one, filter_)
-        if check_data:
+        check_data_bv = await u.db_executor(mongo_db.bili_av.find_one, {'bv': bv_id})
+        if check_data or check_data_bv:
             pass
         else:
-            msg = await get_msg(result['mid'], result['author'], result['title'], av_id, bot)
+            msg = await get_msg(result['mid'], result['author'], result['title'], av_id, bot, bv_id)
             await u.batch_send_msg(bot.config.check_status_list, msg)
-            insert_data = {'av': av_id, 'read': True}
+            if av_id != '-1':
+                insert_data = {'av': av_id, 'read': True, 'bv': bv_id}
+            else:
+                insert_data = {'read': True, 'bv': bv_id}
             await u.db_executor(mongo_db.bili_av.insert, insert_data)
 
 
-async def get_msg(mid, author, title, av_id, bot):
+async def get_msg(mid, author, title, av_id, bot, bv_id):
     if int(mid) in bot.config.white_list:
         mongo_db = bot.config.mongo_db['keientist']
-        insert_data = {'av': av_id, 'title': title, 'author': author, 'ctime': round(time.time())}
+        if av_id != -1:
+            insert_data = {'av': av_id, 'title': title, 'author': author, 'ctime': round(time.time()), 'bv': bv_id}
+        else:
+            insert_data = {'title': title, 'author': author, 'ctime': round(time.time()), 'bv': bv_id}
         await u.db_executor(mongo_db.zhuanfa_list.insert, insert_data)
         msg = "啊啊啊啊啊，这这这这是小妹的新视频，快看啊快看啊\nUP主：{}，标题：{}\nhttps://www.bilibili.com/video/{}\n大家、大家" \
               "、一定一定一定转发转发转发，然后是点赞投币收藏三连\n现在b站的转发进热门的比重是最大最大最大的!"\
-            .format(author, title, av_id)
+            .format(author, title, bv_id)
     else:
         msg = "有新的歌姬相关视频了\nUP主：{}，标题：{}\nhttps://www.bilibili.com/video/{}，快去抢热评" \
-            .format(author, title, av_id)
+            .format(author, title, bv_id)
     return msg
--- bili/plugins/test_zhuanfa.py
+++ bili/plugins/test_zhuanfa.py
@@ -14,11 +14,11 @@ async def _(session: CommandSession):
     content = session.ctx['message'][0]['data']['content']
     try:
         av = content.split('www.bilibili.com/video/')[1].split('/?p=')[0]
-        data = await u.web_get('https://api.bilibili.com/x/web-interface/view?aid={}'.format(av[2:]))
+        data = await u.web_get('https://api.bilibili.com/x/web-interface/view?bvid={}'.format(av[2:]))
         mid = data['data']['owner']['mid']
         if mid in bot.config.zhuanfa:
             await send_feedback(session, data['data']['title'], content, bot)
-    except IndexError:
+    except Exception:
         try:
             title = content.split('"desc":"')[1].split('"')[0]
             await send_feedback(session, title, content, bot, type=1)
@@ -39,7 +39,7 @@ def get_re_str(str_):
 
 async def send_feedback(session, title, content, bot, type=1):
     if check_content(session, content, type):
-        await session.send('盗别人转发的biss')
+        await session.send('[CQ:at,qq={}]盗别人转发的biss'.format(session.ctx['user_id']))
     else:
         mongo_db = bot.config.mongo_db['keientist']
         data_parser = HTMLParser.HTMLParser()
@@ -48,21 +48,23 @@ async def send_feedback(session, title, content, bot, type=1):
         filter_data = {'title': re.compile(re_str)}
         check_data = await u.db_executor(mongo_db.zhuanfa_list.find_one, filter_data)
         if check_data:
+            video_id = check_data['av'] if check_data.get('av') else check_data['bv']
             date = datetime.datetime.fromtimestamp(check_data['ctime'])
             days = datetime.timedelta(days=7)
             if date + days < datetime.datetime.now():
-                await session.send('感谢转发，内容：{}\n不过，这个视频超出7天时间不会被记录啦'.format(title))
+                await session.send('[CQ:at,qq={}]\n感谢转发，内容：{}\n不过，这个视频超出7天时间不会被记录啦'.format(
+                    session.ctx['user_id'], title))
                 return
             success = True
             today = datetime.datetime.now().strftime('%Y-%m-%d')
             filter_user = {'qq': session.ctx['user_id']}
             check_user = await u.db_executor(mongo_db.zhuanfa_user.find_one, filter_user)
             if not check_user:
-                check_user = {'qq': session.ctx['user_id'], 'data': {check_data['av']: {'count': 1, 'uptime': today}}}
+                check_user = {'qq': session.ctx['user_id'], 'data': {video_id: {'count': 1, 'uptime': today}}}
                 await u.db_executor(mongo_db.zhuanfa_user.insert, check_user)
             else:
-                av_data = check_user['data'].get(check_data['av'])
-                count = 2 if check_data['av'] in bot.config.zhuanfa_double else 1
+                av_data = check_user['data'].get(video_id)
+                count = 2 if video_id in bot.config.zhuanfa_double else 1
                 if av_data:
                     if av_data['uptime'] == today:
                         success = False
@@ -70,12 +72,13 @@ async def send_feedback(session, title, content, bot, type=1):
                         av_data['uptime'] = today
                         av_data['count'] += count
                 else:
-                    check_user['data'][check_data['av']] = {'count': count, 'uptime': today}
+                    check_user['data'][video_id] = {'count': count, 'uptime': today}
                 await u.db_executor(mongo_db.zhuanfa_user.save, check_user)
             if success:
-                await session.send('感谢转发，内容：{}'.format(title))
+                await session.send('[CQ:at,qq={}]\n感谢转发，内容：{}'.format(session.ctx['user_id'], title))
             else:
-                await session.send('感谢转发，内容：{}\n不过本机器人一天只记录第一次转发哦'.format(title))
+                await session.send('[CQ:at,qq={}]\n感谢转发，内容：{}\n不过本机器人一天只记录第一次转发哦'.format(
+                    session.ctx['user_id'], title))
 
 
 def check_content(session, content, type):
@@ -97,6 +100,20 @@ async def _(session: CommandSession):
     try:
         title = content.split('"title":"')[1].split('"')[0]
         await send_feedback(session, title, content, bot, type=2)
+    except IndexError:
+        print('111111111111111111111111111')
+        print(content)
+
+
+@on_command('zhuanfazhuanfazhuanfa_hd', only_to_me=False)
+async def _(session: CommandSession):
+    if not u.check_zhuanfa(session):
+        return
+    bot = nonebot.get_bot()
+    content = str(session.ctx['message'])
+    try:
+        title = content.split('text=')[1].split('…,url=')[0]
+        await send_feedback(session, title.strip(), content, bot, type=2)
     except IndexError:
         print('111111111111111111111111111')
         print(content)
@@ -144,4 +161,29 @@ async def _(session: CommandSession):
         msg += '\n本机器人替小妹谢谢你的支持'
     else:
         msg = '要么是QQ号输错了，要么是目前还木有转发记录'
+    await session.send(msg)
+
+
+@on_command('settlement', only_to_me=True)
+async def _(session: CommandSession):
+    bot = nonebot.get_bot()
+    if session.ctx['user_id'] not in bot.config.settle_list:
+        return
+    mongo_db = bot.config.mongo_db['keientist']
+    rank_data = list(await u.db_executor(mongo_db.zhuanfa_user.find, {}))
+    rank_list = [{'qq': rank['qq'], 'count': sum([rank['data'][key]['count'] for key in rank['data'].keys()])}
+                 for rank in rank_data]
+    rank_list.sort(key=lambda x: x['count'], reverse=True)
+    msg = '\n'.join(['QQ: {}, 转发数: {}'.format(rank['qq'], rank['count']) for rank in rank_list])
+    await session.send(msg)
+
+
+@on_command('finish', only_to_me=True)
+async def _(session: CommandSession):
+    bot = nonebot.get_bot()
+    if session.ctx['user_id'] not in bot.config.settle_list:
+        return
+    mongo_db = bot.config.mongo_db['keientist']
+    await u.db_executor(mongo_db.zhuanfa_user.remove, {})
+    msg = '本月转发数据已初始化'
     await session.send(msg)
