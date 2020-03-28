51fec8725dc7afb379687dac5ddc64eaad0d7337
zhuyike <zhuyike@bupt.edu.cn>
2020-03-28 15:55:55 +0800

    save

--- bili/plugins/new_number.py
+++ bili/plugins/new_number.py
@@ -7,4 +7,4 @@ async def _(session: NoticeSession):
     bot = nonebot.get_bot()
     if session.ctx['group_id'] not in bot.config.new_number_list:
         return
-    await session.send('欢迎新战姬众～[CQ:at,qq={}]\n请认真看完置顶的群公告（暴躁老哥模式）\n顺便： https://wj.qq.com/s2/5662973/8258/ live相关的问卷了解一下'.format(session.ctx['user_id']))
+    await session.send('欢迎新战姬众～[CQ:at,qq={}]\n请认真看完置顶的群公告'.format(session.ctx['user_id']))
--- bili/plugins/test.py
+++ bili/plugins/test.py
@@ -12,3 +12,21 @@ async def weather(session: CommandSession):
     follower = str(data['data']['follower'])
     now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     await session.send('截止到{}，粉丝数为{}'.format(now, follower))
+
+
+@on_command('抵制高仿', aliases=('高仿', '缝合姬'), only_to_me=False)
+async def _(session: CommandSession):
+    # if not u.check_1st_control(session):
+    #     return
+    await session.send('为什么要抵制高仿，先是用高仿名字蹭热度、引流，别的高仿号也就评论区抖机灵，这高仿在无授权的情况下发布录播。小妹'
+                       '几次找到他要求改名还置之不理，最后被人举报和谐掉了再改个应援会的名字，美名其曰顾全大局。要求他人改名殊不知自己'
+                       '就是这般无赖')
+
+
+@on_command('引流', only_to_me=False)
+async def _(session: CommandSession):
+    if not u.check_1st_control(session):
+        return
+    await session.send('引流最严重的几次就像鸭鸭的cyberangel，本体播放12万，加上被引流吸血吸掉的5万，就是17万，但要考虑到b站播放越高'
+                       '曝光也越多，这个视频起码能上20万，20万播放的视频能涨多少粉？所以需要大家抵制高仿')
+
